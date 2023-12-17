9f9b47a518446eb561854ebf8074822daba340af
Danjel Hysenaj <danjel.hysenaj@nl.abnamro.com>
2023-12-17 14:59:58 +0100

    added for loop to keep the program open

--- hello.go
+++ hello.go
@@ -13,36 +13,39 @@ func main() {
 	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// ask user for their name
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name:")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email address")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
 
-	fmt.Println("how many tickets do you want to order:")
-	fmt.Scan(&userTickets)
+		fmt.Println("how many tickets do you want to order:")
+		fmt.Scan(&userTickets)
 
-	//userName = "Tom"
-	//userTickets = 2
-	remainingTickets = remainingTickets - userTickets
+		//userName = "Tom"
+		//userTickets = 2
+		remainingTickets = remainingTickets - userTickets
 
-	// now working with dynamic list which will include whatever name surname on the bookings Slice lists in GO
-	bookings = append(bookings, firstName+""+lastName)
+		// now working with dynamic list which will include whatever name surname on the bookings Slice lists in GO
+		bookings = append(bookings, firstName+""+lastName)
 
-	//fmt.Printf("The whole slice: %v\n", bookings)
-	//fmt.Printf("The first value: %v\n", bookings[0])
-	//fmt.Printf("Slice type: %T\n", bookings)
-	//fmt.Printf("Slice length: %v\n", len(bookings))
+		//fmt.Printf("The whole slice: %v\n", bookings)
+		//fmt.Printf("The first value: %v\n", bookings[0])
+		//fmt.Printf("Slice type: %T\n", bookings)
+		//fmt.Printf("Slice length: %v\n", len(bookings))
 
-	fmt.Println("User", firstName, lastName, "booked", userTickets, "tickets", "You will recieve your tickets on mail box", email)
-	fmt.Println("The remaining tickets are:", remainingTickets)
-	fmt.Printf("These are all our bookings: %v\n", bookings)
+		fmt.Println("User", firstName, lastName, "booked", userTickets, "tickets", "You will recieve your tickets on mail box", email)
+		fmt.Println("The remaining tickets are:", remainingTickets)
+		fmt.Printf("These are all our bookings: %v\n", bookings)
+	}
+
 }

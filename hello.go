6c4bc4c01db0e303f7a7c92aa4cdd3579024f15b
Danjel Hysenaj <danjel.hysenaj@nl.abnamro.com>
2023-12-17 15:44:44 +0100

    added if else statement

--- hello.go
+++ hello.go
@@ -38,38 +38,38 @@ func main() {
 		//userTickets = 2
 
 		// check if the user requested more then the remaning tickets on the booking
-		if userTickets > remainingTickets {
+		if userTickets <= remainingTickets {
+			remainingTickets = remainingTickets - userTickets
+
+			// now working with dynamic list which will include whatever name surname on the bookings Slice lists in GO
+			bookings = append(bookings, firstName+" "+lastName)
+
+			//fmt.Printf("The whole slice: %v\n", bookings)
+			//fmt.Printf("The first value: %v\n", bookings[0])
+			//fmt.Printf("Slice type: %T\n", bookings)
+			//fmt.Printf("Slice length: %v\n", len(bookings))
+
+			fmt.Println("User", firstName, lastName, "booked", userTickets, "tickets", "You will recieve your tickets on mail box", email)
+			fmt.Println("The remaining tickets are:", remainingTickets)
+
+			// loop through the strings and print only the first String of the Slice entered
+			// The index is ignored by putting _
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				fmt.Printf("There are no more tickets available for the Conference, Please order one next Year ")
+				break
+			}
+		} else {
 			fmt.Printf("We only have %v tickets remaning, so you can't book %v tickets \n", remainingTickets, userTickets)
-			continue
 		}
 
-		remainingTickets = remainingTickets - userTickets
-
-		// now working with dynamic list which will include whatever name surname on the bookings Slice lists in GO
-		bookings = append(bookings, firstName+" "+lastName)
-
-		//fmt.Printf("The whole slice: %v\n", bookings)
-		//fmt.Printf("The first value: %v\n", bookings[0])
-		//fmt.Printf("Slice type: %T\n", bookings)
-		//fmt.Printf("Slice length: %v\n", len(bookings))
-
-		fmt.Println("User", firstName, lastName, "booked", userTickets, "tickets", "You will recieve your tickets on mail box", email)
-		fmt.Println("The remaining tickets are:", remainingTickets)
-
-		// loop through the strings and print only the first String of the Slice entered
-		// The index is ignored by putting _
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("The first names of bookings are: %v\n", firstNames)
-
-		if remainingTickets == 0 {
-			// end program
-			fmt.Printf("There are no more tickets available for the Conference, Please order one next Year ")
-			break
-		}
 	}
 
 }
